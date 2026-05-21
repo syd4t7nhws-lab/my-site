cab8491540332baab3cc0015c11dbacaf02cf9d3
syd4t7nhws-lab <syd4t7nhws@privaterelay.appleid.com>
2026-05-21 22:49:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function showMessage() {
   alert("お問い合わせありがとうございます！");
 }
+
+const fadeElements = document.querySelectorAll('.fade-up');
+
+window.addEventListener('scroll', () => {
+  fadeElements.forEach(element => {
+    const position = element.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.2;
+
+    if (position < screenPosition) {
+      element.classList.add('show');
+    }
+  });
+});
